4a63e65bf1aaab33c010c1a037f07cac22443145
Aaron Snowberger <jekkilekki@gmail.com>
2016-03-28 17:04:47 +0900

    Update class-jkl-timezones-shortcode.php

--- inc/class-jkl-timezones-shortcode.php
+++ inc/class-jkl-timezones-shortcode.php
@@ -1,8 +1,52 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * The class that creates the plugin Shortcode.
+ * 
+ * Sets up the shortcode and its attributes
+ * 
+ * @since       0.0.1
+ * @package     JKL_Timezones
+ * @subpackage  JKL_Timezones/inc
+ * @author      Aaron Snowberger <jekkilekki@gmail.com>
  */
-
+/* Prevent direct access */
+if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! class_exists( 'JKL_Timezones_Shortcode' ) ) {
+    
+    class JKL_Timezones_Shortcode {
+        
+        /**
+         * Register shortcode with WordPress
+         * 
+         * @since   0.0.1
+         */
+        public function __construct() {
+            
+            $widget_ops = array(
+                    'classname'     => 'jkl_timezones_widget',
+                    'description'   => __( 'A Timezone Calculator Widget to help easily match times in different zones.', 'jkl-timezones' ),
+            );
+            parent::__construct( 
+                    'jkl_timezones_widget',                 // Base ID
+                    __( 'JKL Timezones', 'jkl-timezones' ), // Name
+                    $widget_ops                             // Args
+            );
+            
+            load_plugin_textdomain( 'jkl-timezones', false, basename( dirname( __FILE__) ) . '/languages' );
+            
+        }
+        
+        function jkl_timezones_make_shortcode( $atts ) {
+            
+            $a = shortcode_atts(
+                array(
+                    'color' => 'steelblue',
+                ), $atts );
+            return "color = { $a[ 'color'] }";
+        
+        }
+        add_shortcode( 'jkl_tz', 'jkl_timezones_make_shortcode' );
+        
+    } // END class JKL_Timezones_Shortcode
+    
+} // END if ( ! class_exists() ) 
