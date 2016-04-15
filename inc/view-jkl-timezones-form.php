6f5e1b44c70c71e220a7a587871550be8493fa67
Aaron Snowberger <jekkilekki@gmail.com>
2016-04-16 08:50:53 +0900

    Restyle simpler, more beautiful - post only so far

--- inc/view-jkl-timezones-form.php
+++ inc/view-jkl-timezones-form.php
@@ -16,42 +16,44 @@ require_once( 'controller.php' );
 <form id="jkl_timezones_form" action="" method="POST">
     
     <h4>Timezone Calculator</h4>
-        
-    <dl class="jkl-from-time">
-        <dt>From Time:</dt>
-        <dd>
+    <div class="jkl-from">
+        From:
+        <div class="jkl-from-time">
             <input type="text" name="jkl_tz_from_date" class="jkl-timezones-date" value="<?php
                 echo $from_date;
             ?>"><span class="dashicons dashicons-calendar"></span>
             <select name="jkl_tz_from_time" class="jkltz-time">
                 <?= time_select_options( $from_time ); ?>
             </select>
-            <input type="checkbox" name="jkl_tz_am" id="jkltz-am" <?php echo ( $ampm == 'am' ) ? " checked" : ''; ?>>am
-        </dd>
-    </dl>
-    <dl class="jkl-from-tz jkl-tz-select">
-        <dt>From Timezone:</dt>
-        <dd>
+            <label for="jkltz-am" class="jkl-ampm">
+                <input type="checkbox" name="jkl_tz_am" id="jkltz-am" <?php echo ( $ampm == 'am' ) ? " checked" : ''; ?>>
+                am
+            </label>
+        </div>
+        <div class="jkl-from-tz jkl-tz-select">
             <select name="jkl_tz_from_tz" id="from_continent">
                 <?= timezone_select_options( $from_tz ); ?>
             </select>
-        </dd>
-    </dl>
-    <dl class="jkl-to-tz jkl-tz-select">
-        <dt>To Timezone:</dt>
-        <dd>
+        </div>
+    </div>
+    
+    <div class="jkl-to">
+        To:
+        <div class="jkl-to-time">
+            <input type="text" class="jkl-converted-date" value="<?= 
+                isset( $converted_time ) ? $converted_time->format( 'F j, Y' ) : ''; ?>"
+                readonly>
+            <input type="text" class="jkl-converted-time" value="<?= 
+                isset( $converted_time ) ? $converted_time->format( 'g:i a' ) : ''; ?>"
+                readonly>
+        </div>
+        <div class="jkl-to-tz jkl-tz-select">
             <select name="jkl_tz_to_tz" id="to_continent">
                 <?= timezone_select_options( $to_tz ); ?>
             </select>
-        </dd>
-    </dl>
-    <?php if ( isset( $converted_time ) ) : ?>
-    <dl class="jkl-converted-time">
-        <dt>Converted Time:</dt>
-        <dd><?= $converted_time->format( 'F j, Y \a\t g:i a' ); ?></dd>
-    </dl>
-    <?php endif; ?>
-
+        </div>
+    </div>
+    <hr>
     <div class="jkl-tz-controls">
         <input type="submit" name="jkl_tz_clear" value="Reset">
         <input type="submit" name="jkl_tz_submit" value="Go">
